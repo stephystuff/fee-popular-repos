20ed6fc734516e656c722434a347b1f161a93776
StephyStuff <stephanie.birch13@gmail.com>
2017-01-16 10:10:21 -0500

    Modified block of code for popularity filter. Added code to sort repo popularity.

---
+++ repos/popularity.filter.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  angular.module('gh')
+    .filter('popularity', PopularityFilter);
+
+  function PopularityFilter() {
+    var popularity = input.map(function getPopularity(repo) {
+      repo.popularity = input.stargazers_count + ((2 * repo.forks_count) + (repo.open_issues_count/2));
+      return repo;
+    });
+
+    popularity = popularity.sort(function (a, b) {
+      if (a.popularity > b.popularity) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    return popularity;
+  }
+
+}());
