c3d9e06e280b3feb4e6b91af8e6e689047c7b4f2
StephyStuff <stephanie.birch13@gmail.com>
2017-01-16 16:15:56 -0500

    Added return function for sorting popularity.

--- repos/popularity.filter.js
+++ repos/popularity.filter.js
@@ -5,19 +5,22 @@
     .filter('popularity', PopularityFilter);
 
   function PopularityFilter() {
-    var popularity = input.map(function getPopularity(repo) {
-      repo.popularity = input.stargazers_count + ((2 * repo.forks_count) + (repo.open_issues_count/2));
-      return repo;
-    });
 
-    popularity = popularity.sort(function (a, b) {
-      if (a.popularity > b.popularity) {
-        return -1;
-      } else {
-        return 1;
-      }
-    });
-    return popularity;
+    return function sortByPopularity(input) {
+      var popularity = input.map(function getPopularity(repo) {
+        repo.popularity = input.stargazers_count + ((2 * repo.forks_count) + (repo.open_issues_count/2));
+        return repo;
+      });
+
+      popularity = popularity.sort(function (a, b) {
+        if (a.popularity > b.popularity) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      return popularity;
+    };
   }
 
 }());
